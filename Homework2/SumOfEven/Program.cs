6bc0d973b7969cf2a09fab658e1c52cb84beeebd
Dinko <98716869+DinelHasic@users.noreply.github.com>
2022-03-16 15:44:04 +0100

    Add files via upload

---
+++ Homework2/SumOfEven/Program.cs
@@ -0,0 +1,31 @@
+﻿
+int[] numbers = new int[6];
+int count = 0;
+int sum = 0;
+
+while (numbers.Length > count)
+{
+    Console.WriteLine("Enter a number:");
+    string numberString = Console.ReadLine();
+
+    bool isParse = int.TryParse(numberString, out int numberParse);
+    if (isParse)
+    {
+        numbers[count] = numberParse;
+        count++;
+    }
+    else
+    {
+        Console.WriteLine("Can't convert a word to a integer try again:");
+    }
+
+}
+
+
+foreach (int num in numbers)
+{
+    if (num % 2 == 0)
+        sum += num;
+}
+
+Console.WriteLine($" Sum of even numbers: {sum}");
