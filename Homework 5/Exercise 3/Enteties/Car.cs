c680947dd5df8155e5b014b9fafb0f48f366b21a
Dinko <98716869+DinelHasic@users.noreply.github.com>
2022-04-01 22:20:24 +0200

    Add files via upload

---
+++ Homework 5/Exercise 3/Enteties/Car.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise_3.Enteties
+{
+    class Car
+    {
+        public int Id { get; set; }
+        public string BrandName { get; set; }
+        public string ModelName { get; set; }
+        public Color Color { get; set; }
+        public int DistanceTraveled { get; set; }
+        public DateTime DateofModel { get; set; }
+
+        public Car(int id, string brandName, string modelName, Color color, int distanceTraveled, DateTime dateofModel)
+        {
+            Id = id;
+            BrandName = brandName;
+            ModelName = modelName;
+            Color = color;
+            DistanceTraveled = distanceTraveled;
+            DateofModel = dateofModel;
+        }
+
+        public void CalcualtePrice()
+        {
+            int price = DistanceTraveled * DateofModel.Year / 3;
+
+            Console.WriteLine($" The price of the car: {price}$");
+        }
+    }
+}
---
+++ Homework 5/Exercise 3/Enteties/Color.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise_3.Enteties
+{
+    enum Color
+    {
+        Black,
+        Red,
+        Green,
+        White
+    }
+}
