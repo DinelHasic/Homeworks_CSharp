a33d4f1bb8c8786b890897f9ce4e66cae5ea2156
Dinko <98716869+DinelHasic@users.noreply.github.com>
2022-04-01 22:19:37 +0200

    Add files via upload

---
+++ Homework 5/Exercise 2/User/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise_2
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime CreateOn { get; set; }
+
+        public User(int id, string firstName, string lastName, DateTime createOn)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            CreateOn = createOn;
+        }
+    }
+}
